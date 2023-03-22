09a64293a5132240eed34ea92a7c656f1a9147aa
Marcelo Lima <m-lima@users.noreply.github.com>
2023-03-22 13:46:52 +0100

    Add help message

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/manifoldco/promptui"
 )
+
+func printUsage(writer io.Writer) {
+	fmt.Fprintln(writer, "Usage: msaler [client | COMMAND] [options...]")
+	fmt.Fprintln(writer)
+	fmt.Fprintln(writer, "A command-line manager for MSAL clients")
+	fmt.Fprintln(writer)
+	fmt.Fprintln(writer, "Commands:")
+	fmt.Fprintln(writer, "  token  [client] [-v]  Generate an oauth token for a client")
+	fmt.Fprintln(writer, "  new                   Register a new client")
+	fmt.Fprintln(writer, "  delete [client]       Delete a registered client")
+	fmt.Fprintln(writer, "  print  [client]       Print the client information")
+	fmt.Fprintln(writer, "  config                Print the path to the configuration file containing the registered clients")
+	fmt.Fprintln(writer, "  help                  Print this help message")
+	fmt.Fprintln(writer)
+	fmt.Fprintln(writer, "Options:")
+	fmt.Fprintln(writer, "  client                The client to use. If ommited, a selection menu will be presented")
+	fmt.Fprintln(writer, "  -v                    Print extra token fields to stderr")
+}
 
 func main() {
 	var err error
@@ -12,7 +33,9 @@ func main() {
 	} else {
 		modeParam := os.Args[1]
 
-		if "token" == modeParam {
+		if "help" == modeParam {
+			printUsage(os.Stdout)
+		} else if "token" == modeParam {
 			err = GetClientToken(os.Args[2:])
 		} else if "new" == modeParam {
 			err = NewClient(os.Args[2:])
@@ -34,8 +57,9 @@ func main() {
 		}
 	}
 
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+	if err != nil && err != promptui.ErrEOF && err != promptui.ErrAbort && err != promptui.ErrInterrupt {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		printUsage(os.Stderr)
 		os.Exit(-1)
 	}
 }
